008e94267fb8b35716e06232f53e986c2994dffb
abreham1222 <abreham1222@gmail.com>
2022-09-06 01:28:16 +0300

    fix linter

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const humburger = document.querySelector('.humburger');
 const navMenu = document.querySelector('.navbar-desk');
 
 humburger.addEventListener('click', () => {
-    humburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  humburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
-  
+
 document.querySelectorAll('.nav-lin').forEach((n) => n.addEventListener('click', () => {
-    humburger.classList.remove('active');
-    navMenu.classList.remove('active');
+  humburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
-  
