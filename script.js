b619a06b935aa2112791021c16c8249617b6d243
abreham1222 <abreham1222@gmail.com>
2022-09-05 22:38:16 +0300

    added active event listner

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const navMenu = document.querySelector('.navbar-desk');
 humburger.addEventListener('click', () => {
     humburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-  });
+});
+  
+document.querySelectorAll('.nav-lin').forEach((n) => n.addEventListener('click', () => {
+    humburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
   
