4717f5483515fea29dfb69330261534170b4c467
abreham1222 <abreham1222@gmail.com>
2022-09-05 22:37:22 +0300

    added eventlistner to humburger

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const humburger = document.querySelector('.humburger');
 const navMenu = document.querySelector('.navbar-desk');
+
+humburger.addEventListener('click', () => {
+    humburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+  
